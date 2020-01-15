0c95e2c75624bd0b77a9d2e8edee85d34163bba9
Saket Lunker <35565062+saketlunker@users.noreply.github.com>
2020-01-15 12:56:31 +0530

    Update 2020

--- 2019/js/scripts.js
+++ 2019/js/scripts.js
@@ -2,7 +2,6 @@ var html = document.querySelector("html");
 var toggleOpen = document.getElementById('toggle-open');
 var toggleClose = document.getElementById('toggle-close');
 var navMenu = document.getElementsByClassName('nav-menu')[0];
-//var card = document.querySelectorAll('.card');
 
 html.addEventListener('click', function () {
   navMenu.classList.remove('open');
@@ -17,8 +16,3 @@ toggleClose.addEventListener('click', function (e) {
   e.stopPropagation();
   navMenu.classList.toggle('open');
 });
-
-// card.forEach(c => {
-//   c.addEventListener('click', function () {
-//   });
-// });
